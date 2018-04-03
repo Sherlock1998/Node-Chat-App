1d5e66456ab07f4ddbbf75652559b2854ca76492
Sherlock1998 <iuncleblade_yj@hotmail.com>
2018-04-03 23:04:39 +0800

    Added form for messages & show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -7,11 +7,24 @@ socket.on('disconnect', function () {
   console.log('Disconnected to the server');
 });
 
-// Emits || Client > Server
-
-
-// Listens || Server > Client
 socket.on('newMessage', function (message) {
   console.log('New message', message);
+  const li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text} `);
+  jQuery('#messages').append(li);
 });
 
+socket.emit('createMessage', {
+  from: 'The One',
+  text: 'hello',
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val(),
+  }, function () {
+    console.log('Got it');
+  });
+});
